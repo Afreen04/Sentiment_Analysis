9651dd37ca4c392f3a8c05824f3f7ab58c58a8c5
Afreen04 <afreenwins@gmail.com>
2016-04-17 12:24:35 +0530

    Sentiment.py runs tweepy for pulling the tweets and uses textblob to classify sentiments

---
+++ sentiment.py
@@ -0,0 +1,89 @@
+import json
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+from textblob import TextBlob
+from elasticsearch import Elasticsearch
+import logging
+
+# import twitter keys and tokens
+from config import *
+
+
+# create instance of elasticsearch
+es = Elasticsearch()
+
+tracer = logging.getLogger('elasticsearch.trace')
+tracer.setLevel(logging.INFO)
+tracer.addHandler(logging.FileHandler('/tmp/es_trace.log'))
+
+
+
+class TweetStreamListener(StreamListener):
+
+    # on success
+    def on_data(self, data):
+
+        # decode json
+        dict_data = json.loads(data)
+
+        # pass tweet into TextBlob
+        tweet = TextBlob(dict_data["text"])
+
+        #print tweet
+        print tweet
+
+        # output sentiment polarity
+        print tweet.sentiment.polarity
+
+        # determine if sentiment is positive, negative, or neutral
+        if tweet.sentiment.polarity < 0:
+            sentiment = "negative"
+        elif tweet.sentiment.polarity == 0:
+            sentiment = "neutral"
+        else:
+            sentiment = "positive"
+
+        # output sentiment
+        print sentiment
+
+        
+	"""
+        # add text and sentiment info to elasticsearch
+        es.index(index="sentiment",
+                 doc_type="test-type",
+                 body={"author": dict_data["user"]["screen_name"],
+                       "date": dict_data["created_at"],
+                       "message": dict_data["text"],
+                       "polarity": tweet.sentiment.polarity,
+                       "subjectivity": tweet.sentiment.subjectivity,
+                       "sentiment": sentiment})
+        
+
+
+        return True
+	"""
+
+
+    # on failure
+    def on_error(self, status):
+        if status_code == 420:
+            #returning False in on_data disconnects the stream
+            return False
+        print status
+
+if __name__ == '__main__':
+
+    # create instance of the tweepy tweet stream listener
+    listener = TweetStreamListener()
+
+    # set twitter keys/tokens
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_key, access_secret)
+
+    # create instance of the tweepy stream
+    stream = Stream(auth, listener)
+
+    keyword = raw_input("Input keyword: ")
+    # search twitter for keyword
+    stream.filter(track=[keyword])
